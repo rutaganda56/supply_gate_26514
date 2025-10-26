97686805802f0a68a96ebbe0577036b644f4990e
Rutaganda <valentinrutaganda04@gmail.com>
2025-10-26 14:44:30 +0300

    shared supply gate application to github

---
+++ src/main/java/org/example/supply_gate_26514/SupplyGate26514Application.java
@@ -0,0 +1,13 @@
+package org.example.supply_gate_26514;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyGate26514Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupplyGate26514Application.class, args);
+    }
+
+}
