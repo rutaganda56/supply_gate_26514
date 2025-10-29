5cf6e69be24929ec5b716f39da5b3a8895963a11
Rutaganda <valentinrutaganda04@gmail.com>
2025-10-29 20:42:51 +0300

    added attributes to product entity

--- src/main/java/org/example/supply_gate_26514/model/Product.java
+++ src/main/java/org/example/supply_gate_26514/model/Product.java
@@ -1,4 +1,61 @@
 package org.example.supply_gate_26514.model;
 
-public class Products {
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID productId;
+
+    public Product() {
+    }
+
+    public UUID getProductId() {
+        return productId;
+    }
+
+    public void setProductId(UUID productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public Double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(Double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    private String productName;
+    private String productDescription;
+    private Double productPrice;
+    private String quantity;
 }
