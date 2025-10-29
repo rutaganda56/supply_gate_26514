1281b8306fb595d46446beeb3043ff2bf713e2a2
Rutaganda <valentinrutaganda04@gmail.com>
2025-10-29 20:37:06 +0300

    added attributes to Image entity

--- src/main/java/org/example/supply_gate_26514/model/Image.java
+++ src/main/java/org/example/supply_gate_26514/model/Image.java
@@ -1,4 +1,33 @@
 package org.example.supply_gate_26514.model;
 
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table
 public class Image {
+    public UUID getImageId() {
+        return imageId;
+    }
+
+    public Image() {
+    }
+
+    public void setImageId(UUID imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID imageId;
+    private String imageUrl;
 }
