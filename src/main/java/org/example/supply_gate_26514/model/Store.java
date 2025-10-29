adb47ef160f6d07b5531b5ebdc7589c6d1026903
Rutaganda <valentinrutaganda04@gmail.com>
2025-10-29 20:44:39 +0300

    added attributes to store entity

--- src/main/java/org/example/supply_gate_26514/model/Store.java
+++ src/main/java/org/example/supply_gate_26514/model/Store.java
@@ -1,4 +1,33 @@
 package org.example.supply_gate_26514.model;
 
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "stores")
 public class Store {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID storeId;
+    private String storeName;
+
+    public UUID getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(UUID storeId) {
+        this.storeId = storeId;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public Store() {
+    }
 }
