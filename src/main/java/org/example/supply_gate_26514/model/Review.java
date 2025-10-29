54a4124690312258cf156b4291bf64a15f6bf5a6
Rutaganda <valentinrutaganda04@gmail.com>
2025-10-29 20:43:49 +0300

    added an attribute to review entity

--- src/main/java/org/example/supply_gate_26514/model/Review.java
+++ src/main/java/org/example/supply_gate_26514/model/Review.java
@@ -1,4 +1,35 @@
 package org.example.supply_gate_26514.model;
 
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "reviews")
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID reviewId;
+
+    public Review() {
+    }
+
+    public UUID getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(UUID reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    private String message;
+
 }
