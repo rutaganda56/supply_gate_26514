49e39cc9d6cf28b3c786d8f66e31fe98ae97476f
Rutaganda <valentinrutaganda04@gmail.com>
2025-10-29 20:34:27 +0300

    added attributes for category entity

--- src/main/java/org/example/supply_gate_26514/model/Category.java
+++ src/main/java/org/example/supply_gate_26514/model/Category.java
@@ -1,4 +1,33 @@
 package org.example.supply_gate_26514.model;
 
+import jakarta.persistence.*;
+
+import java.util.UUID;
+@Entity
+@Table(name ="categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID categoryId;
+    @Enumerated(EnumType.STRING)
+    private CategoryEnum categoryType;
+
+    public UUID getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(UUID categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public CategoryEnum getCategoryType() {
+        return categoryType;
+    }
+
+    public void setCategoryType(CategoryEnum categoryType) {
+        this.categoryType = categoryType;
+    }
+
+    public Category() {
+    }
 }
