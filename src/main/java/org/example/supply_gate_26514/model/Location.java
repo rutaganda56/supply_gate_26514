a334eb4db20cb5d41d91e02b58d41e2c0a2b543e
Rutaganda <valentinrutaganda04@gmail.com>
2025-10-29 20:40:56 +0300

    added attributes to  location entity

--- src/main/java/org/example/supply_gate_26514/model/Location.java
+++ src/main/java/org/example/supply_gate_26514/model/Location.java
@@ -1,17 +1,8 @@
 package org.example.supply_gate_26514.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.util.UUID;
-@Setter
-@Getter
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "Administative_structure")
 public class Location {
     @Id
@@ -24,4 +15,56 @@ public class Location {
     private  Location parent;
     @Enumerated(EnumType.STRING)
     private LocationEnum locationEnum;
+
+    public UUID getStructureId() {
+        return structureId;
+    }
+
+    public String getStructureCode() {
+        return structureCode;
+    }
+
+    public String getStructureName() {
+        return structureName;
+    }
+
+    public Location getParent() {
+        return parent;
+    }
+
+    public LocationEnum getLocationEnum() {
+        return locationEnum;
+    }
+
+    public void setStructureId(UUID structureId) {
+        this.structureId = structureId;
+    }
+
+    public void setStructureCode(String structureCode) {
+        this.structureCode = structureCode;
+    }
+
+    public void setStructureName(String structureName) {
+        this.structureName = structureName;
+    }
+
+    public void setParent(Location parent) {
+        this.parent = parent;
+    }
+
+    public void setLocationEnum(LocationEnum locationEnum) {
+        this.locationEnum = locationEnum;
+    }
+
+    public Location() {
+    }
+
+    public Location(UUID structureId, String structureCode, String structureName, Location parent, LocationEnum locationEnum) {
+        this.structureId = structureId;
+        this.structureCode = structureCode;
+        this.structureName = structureName;
+        this.parent = parent;
+        this.locationEnum = locationEnum;
+    }
 }
+
