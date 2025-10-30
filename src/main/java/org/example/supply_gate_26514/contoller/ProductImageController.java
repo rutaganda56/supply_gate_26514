aad4806c11521bb2ac031575370481c9bb1061dc
Rutaganda <valentinrutaganda04@gmail.com>
2025-10-30 23:52:30 +0300

    added an exception handler in Product image controller

--- src/main/java/org/example/supply_gate_26514/contoller/ProductImageController.java
+++ src/main/java/org/example/supply_gate_26514/contoller/ProductImageController.java
@@ -5,8 +5,13 @@ import org.example.supply_gate_26514.dto.ProductImageDto;
 import org.example.supply_gate_26514.dto.ProductImageResponseDto;
 import org.example.supply_gate_26514.service.ProductImageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
@@ -32,6 +37,17 @@ public class ProductImageController {
     @DeleteMapping("/deleteImage/{imageId}")
     public void deleteImage(@PathVariable UUID imageId){
         imageService.deleteProductImage(imageId);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        var errors = new HashMap<String, String>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            var fieldName = ((FieldError) error).getField();
+            var errorMsg = error.getDefaultMessage();
+            errors.put(fieldName, errorMsg);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+
     }
 
 }
