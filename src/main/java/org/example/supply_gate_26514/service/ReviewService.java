e2399d7e0c34a2cc5d40f6e38dd5987879bceb27
Rutaganda <valentinrutaganda04@gmail.com>
2025-10-30 17:08:12 +0300

    added a service class to manage business logic for the review entity

---
+++ src/main/java/org/example/supply_gate_26514/service/ReviewService.java
@@ -0,0 +1,55 @@
+package org.example.supply_gate_26514.service;
+
+import org.example.supply_gate_26514.dto.ReviewDto;
+import org.example.supply_gate_26514.dto.ReviewResponseDto;
+import org.example.supply_gate_26514.mapper.ReviewMapper;
+import org.example.supply_gate_26514.model.Product;
+import org.example.supply_gate_26514.model.Review;
+import org.example.supply_gate_26514.model.User;
+import org.example.supply_gate_26514.repository.ProductRepository;
+import org.example.supply_gate_26514.repository.ReviewRepository;
+import org.example.supply_gate_26514.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private ReviewMapper reviewMapper;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<ReviewResponseDto> retrieveAllReviews() {
+        return reviewRepository.findAll().stream().map(reviewMapper::transformToReviewResponseDto).collect(Collectors.toList());
+    }
+    public ReviewResponseDto addAReview(ReviewDto reviewDto) {
+        var review=reviewMapper.transformToReviewDto(reviewDto);
+        var savedReview= reviewRepository.save(review);
+        return reviewMapper.transformToReviewResponseDto(savedReview);
+    }
+    public ReviewResponseDto updateAReview(UUID id, ReviewDto reviewDto) {
+        var existingReview=reviewRepository.findById(id).orElse(new Review());
+        existingReview.setMessage(reviewDto.message());
+        var newUser= userRepository.findById(id).orElse(new User());
+        existingReview.setUser(newUser);
+        var newProduct=productRepository.findById(id).orElse(new Product());
+        existingReview.setProduct(newProduct);
+        var updatedReview= reviewRepository.save(existingReview);
+        return reviewMapper.transformToReviewResponseDto(reviewRepository.save(updatedReview));
+
+    }
+    public String deleteAReview(UUID id) {
+        reviewRepository.deleteById(id);
+        return "Review deleted successfully";
+    }
+}
