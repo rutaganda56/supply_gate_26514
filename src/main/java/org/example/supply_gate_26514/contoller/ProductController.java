6d417887fbf1c0b146629302c5681c1275dfc64c
Rutaganda <valentinrutaganda04@gmail.com>
2025-10-30 17:31:18 +0300

    made crud operations endpoints to  manage products

---
+++ src/main/java/org/example/supply_gate_26514/contoller/ProductController.java
@@ -0,0 +1,50 @@
+package org.example.supply_gate_26514.contoller;
+
+import org.example.supply_gate_26514.dto.ProductDto;
+import org.example.supply_gate_26514.dto.ProductResponseDto;
+import org.example.supply_gate_26514.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("getProducts")
+    public List<ProductResponseDto> getProducts() {
+        return productService.getAllProducts();
+    }
+    @PostMapping("createAProduct")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponseDto createProduct(@RequestBody ProductDto productDto) {
+        return productService.addAProduct(productDto);
+    }
+    @PutMapping("{id}")
+    public ProductResponseDto updateProduct(@PathVariable("id") UUID id, @RequestBody ProductDto productDto) {
+        return productService.updateProduct(id,productDto);
+    }
+    @DeleteMapping("deleteProduct/{id}")
+    public void deleteProduct(@PathVariable("id") UUID id) {
+        productService.deleteProduct(id);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        var errors = new HashMap<String, String>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            var fieldName = ((FieldError) error).getField();
+            var errorMsg = error.getDefaultMessage();
+            errors.put(fieldName, errorMsg);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+}
