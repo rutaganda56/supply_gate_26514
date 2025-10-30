6e38786319519481ea6c0de6fe6b7a622e48c559
Rutaganda <valentinrutaganda04@gmail.com>
2025-10-30 07:25:20 +0300

    added relationships to category entity

--- src/main/java/org/example/supply_gate_26514/model/Category.java
+++ src/main/java/org/example/supply_gate_26514/model/Category.java
@@ -1,7 +1,10 @@
 package org.example.supply_gate_26514.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 
+import java.util.List;
 import java.util.UUID;
 @Entity
 @Table(name ="categories")
@@ -11,6 +14,9 @@ public class Category {
     private UUID categoryId;
     @Enumerated(EnumType.STRING)
     private CategoryEnum categoryType;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    @JsonManagedReference("category-product")
+    private List<Product> product;
 
     public UUID getCategoryId() {
         return categoryId;
