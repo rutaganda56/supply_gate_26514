1e50da580ae1f456a638baeabe00c7650048e3a3
Rutaganda <valentinrutaganda04@gmail.com>
2025-10-30 15:38:29 +0300

    added a repository class for product  entity

---
+++ src/main/java/org/example/supply_gate_26514/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package org.example.supply_gate_26514.repository;
+
+import org.example.supply_gate_26514.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+}
