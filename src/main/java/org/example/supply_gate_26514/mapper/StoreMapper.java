dcf813d4470315327016e51ad15c45cfb5db524b
Rutaganda <valentinrutaganda04@gmail.com>
2025-10-30 15:52:55 +0300

    added a mapper class for mapping store inforrmation

---
+++ src/main/java/org/example/supply_gate_26514/mapper/StoreMapper.java
@@ -0,0 +1,25 @@
+package org.example.supply_gate_26514.mapper;
+
+import org.example.supply_gate_26514.dto.StoreDto;
+import org.example.supply_gate_26514.dto.StoreResponseDto;
+import org.example.supply_gate_26514.model.Store;
+import org.example.supply_gate_26514.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StoreMapper {
+
+    public Store transformStoreToStoreDto(StoreDto storeDto) {
+        Store store = new Store();
+        store.setStoreName(store.getStoreName());
+        store.setStoreEmail(store.getStoreEmail());
+        store.setPhoneNumber(store.getPhoneNumber());
+        User user=new User();
+        user.setUserId(storeDto.userId());
+        store.setUser(user);
+        return store;
+    }
+    public StoreResponseDto transformStoreDtoToStoreResponseDto(Store store) {
+        return new StoreResponseDto(store.getStoreName());
+    }
+}
