bd97680a5d9bd8a52c6c581610fd260626c5802a
Rutaganda <valentinrutaganda04@gmail.com>
2025-11-03 14:52:06 +0200

    added a class to provide the login information

--- src/main/java/org/example/supply_gate_26514/UserRelatedInfo.java
+++ src/main/java/org/example/supply_gate_26514/UserRelatedInfo.java
@@ -1,4 +1,26 @@
 package org.example.supply_gate_26514;
 
-public class UserRelatedInfo {
+import org.example.supply_gate_26514.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserRelatedInfo implements UserDetails {
+  private User user;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getUsername();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getPassword();
+    }
 }
