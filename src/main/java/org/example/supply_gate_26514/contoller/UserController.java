62c33029b318504019d8a9f6421dbba379a4fa78
Rutaganda <valentinrutaganda04@gmail.com>
2025-12-01 19:09:32 +0200

    made some changes on user controller

--- src/main/java/org/example/supply_gate_26514/contoller/UserController.java
+++ src/main/java/org/example/supply_gate_26514/contoller/UserController.java
@@ -3,6 +3,7 @@ package org.example.supply_gate_26514.contoller;
 import jakarta.validation.Valid;
 import org.example.supply_gate_26514.dto.UserDto;
 import org.example.supply_gate_26514.dto.UserResponseDto;
+import org.example.supply_gate_26514.model.User;
 import org.example.supply_gate_26514.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,12 +18,17 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("api/users")
+@RequestMapping("api/auth")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping(value = "/addAUser", consumes = "application/json" , produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping("/login")
+    public String login(@RequestBody User user) {
+        return userService.login(user);
+    }
+
+    @PostMapping(value = "/register", consumes = "application/json" , produces = MediaType.APPLICATION_JSON_VALUE)
     public UserResponseDto addUser(@RequestBody @Valid UserDto userDto) {
         return userService.registerUser(userDto);
     }
