00f00b224100bd887866ad107b28c6ea4c4da010
Rutaganda <valentinrutaganda04@gmail.com>
2025-12-01 19:08:38 +0200

    added a security configutation entity

--- src/main/java/org/example/supply_gate_26514/config/SecurityConfig.java
+++ src/main/java/org/example/supply_gate_26514/config/SecurityConfig.java
@@ -1,7 +1,71 @@
 package org.example.supply_gate_26514.config;
 
+import jakarta.servlet.annotation.WebServlet;
+import org.example.supply_gate_26514.filter.JWTFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@WebServlet
 public class SecurityConfig {
+    @Autowired
+    private JWTFilter jwtFilter;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(customizer-> customizer.disable())
+                .authorizeHttpRequests(request->request
+                        .requestMatchers("api/auth/register","api/location/addOrganisationStructure","api/auth/login","/v2/api-docs"
+                                , "/v3/api-docs"
+
+                                , "/v3/api-docs/**",
+
+                                "/swagger-resources",
+
+                                "/swagger-resources/**",
+
+                                "/configuration/ui",
+
+                                "/configuration/security",
+
+                                "/swagger-ui/**",
+
+                                "/webjars/**",
+
+                                "/swagger-ui.html)")
+                        .permitAll().anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
 }
