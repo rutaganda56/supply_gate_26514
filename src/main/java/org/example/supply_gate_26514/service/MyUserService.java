2ff3e675b15c62b74c1b997d257da1ef750ef05f
Rutaganda <valentinrutaganda04@gmail.com>
2025-12-01 19:13:04 +0200

    added a function to load users by username in MyUserService entity

--- src/main/java/org/example/supply_gate_26514/service/MyUserService.java
+++ src/main/java/org/example/supply_gate_26514/service/MyUserService.java
@@ -1,4 +1,24 @@
 package org.example.supply_gate_26514.service;
 
-public class MyUserService {
+import org.example.supply_gate_26514.UserRelatedInfo;
+import org.example.supply_gate_26514.model.User;
+import org.example.supply_gate_26514.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepo;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user=userRepo.findByUsername(username);
+        if(user==null){
+            System.out.println("user not found");
+        }
+        return new UserRelatedInfo(user);
+    }
 }
